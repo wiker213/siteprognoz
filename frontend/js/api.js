b67fb9fdfa12ab6a65377743a0b4a61c8fb5016c
wiker213 <sswikerss@gmail.com>
2026-05-23 13:47:43 +0300

    fix api url

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://siteprognoz-1.onrender.com";
 
 async function apiRequest(endpoint, options = {}) {
   const res = await fetch(API_URL + endpoint, {
